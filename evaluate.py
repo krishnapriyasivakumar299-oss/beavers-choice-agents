feffe1e096a5fd57824290b6a3396da8450903d5
krishnapriyasivakumar299-oss <krishnapriyasivakumar299@gmail.com>
2026-04-19 19:39:32 +0530

    Created using Colab

---
+++ evaluate.py
@@ -0,0 +1,30 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNHNG3LzYgLMnzBPlpWPWj+",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/krishnapriyasivakumar299-oss/beavers-choice-agents/blob/main/evaluate.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    }
+  ]
+}
